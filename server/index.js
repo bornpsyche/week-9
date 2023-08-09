bef188c61967496a66db6efd5c2c99d3e6022adb
bornpsyche <bornpsyche@gmail.com>
2023-08-09 10:39:42 +0530

    modified index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req,res) => {
-  res.send("Hello World! This should be working now. God Please /\");
+  res.send("Hello World! This should be working now. God Please");
 })
 
 app.get("/me", (req,res) => {
