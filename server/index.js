84d90aa96d192786c2bf1838448b4a5b4de9cd6e
Ashish Pradhan <bornpsyche@gmail.com>
2023-08-09 10:15:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req,res) => {
-  res.send("Hello World! I loved learning to deploy this:)");
+  res.send("Hello World! Why is it not working!");
 })
 
 app.get("/me", (req,res) => {
